073c68cf37c0abbb9ca403bdfa915905fc40d41b
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-07-16 15:46:28 +0300

    add step annotation on base test

--- src/test/java/ru/tinkoff/tests/BaseTest.java
+++ src/test/java/ru/tinkoff/tests/BaseTest.java
@@ -2,10 +2,10 @@ package ru.tinkoff.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.tinkoff.helpers.Attach;
 
@@ -15,14 +15,14 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
     @BeforeAll
-    @DisplayName("Настройка окружения")
+    @Step("Настройка окружения")
     static void setUp() {
-        SelenideLogger.addListener("allure",new AllureSelenide());
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.remote = System.getProperty("selenoid");
-        Configuration.baseUrl = System.getProperty("baseUrl","https://tinkoff.ru");
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://tinkoff.ru");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
         //Configuration.headless = true;
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -34,7 +34,7 @@ public class BaseTest {
     }
 
     @AfterEach
-    @DisplayName("Добавление вложений")
+    @Step("Добавление вложений")
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
