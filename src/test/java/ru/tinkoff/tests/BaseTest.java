e05199e4b5dc0f039d2aa4ff42e94a15a1b59a0f
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2023-07-16 15:34:27 +0300

    add some tests

--- src/test/java/ru/tinkoff/tests/BaseTest.java
+++ src/test/java/ru/tinkoff/tests/BaseTest.java
@@ -1,27 +1,45 @@
 package ru.tinkoff.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.tinkoff.helpers.Attach;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseTest {
     @BeforeAll
+    @DisplayName("Настройка окружения")
     static void setUp() {
+        SelenideLogger.addListener("allure",new AllureSelenide());
+        Configuration.remote = System.getProperty("selenoid");
+        Configuration.baseUrl = System.getProperty("baseUrl","https://tinkoff.ru");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
         //Configuration.headless = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        //Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
+    @DisplayName("Добавление вложений")
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
-
         closeWebDriver();
     }
 }
